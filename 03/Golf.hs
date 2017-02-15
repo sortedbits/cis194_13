a4ea6dedda889d5bee396c6bb62dd12d196dc117
sortedbits <sortedbit@zoho.com>
2017-02-15 12:27:53 +0100

    init

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -13,3 +13,13 @@ get xs n
   | otherwise      = head (drop (n - 1) xs) : get (drop n xs) n  
 
 -- e2 local maxima
+localMaxima :: [Integer] -> [Integer]
+localMaxima []             = []
+localMaxima (x1 : [])      = []
+localMaxima (x1 : x2 : []) = []
+localMaxima (x1 : x2 : x3 : xs)
+  | x1 < x2 && x2 > x3 = x2 : localMaxima (x2 : x3 : xs)
+  | otherwise          = localMaxima (x2 : x3 : xs)
+
+-- e3 histogram
+-- histogram :: [Integer] -> String
