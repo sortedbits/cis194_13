eca61e5798695cd1fdfd815e4d74353399bfb7c0
sortedbits <sortedbit@zoho.com>
2017-02-16 22:44:59 +0100

    init

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -22,4 +22,27 @@ localMaxima (x1 : x2 : x3 : xs)
   | otherwise          = localMaxima (x2 : x3 : xs)
 
 -- e3 histogram
--- histogram :: [Integer] -> String
+histogram :: [Integer] -> String
+histogram xs = plotLine counts (maximum counts)
+  where counts = (countAll xs)
+
+plotLine :: [Integer] -> Integer -> String
+plotLine counts 0 = "==========\n0123456789\n"
+plotLine counts n = (doPlotLine counts n) ++ "\n" ++ (plotLine counts (n - 1))
+
+doPlotLine :: [Integer] -> Integer -> String
+doPlotLine [] _       = ""
+doPlotLine (c : cs) n = (plotChar c n) ++ (doPlotLine cs n)
+
+plotChar :: Integer -> Integer -> String
+plotChar x n = if x >= n then "*"  else " "
+
+countAll :: [Integer] -> [Integer]
+countAll [] = replicate 10 0
+countAll xs = map (countSingle xs) [0..9]
+
+countSingle :: [Integer] -> Integer -> Integer
+countSingle [] _ = 0
+countSingle (x : xs) n
+  | n == x    = 1 + countSingle xs n
+  | otherwise = countSingle xs n
