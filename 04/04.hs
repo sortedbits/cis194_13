bdffe876269c59ae51e03e7a7fe49144ae6de6c9
sortedbits <sortedbit@zoho.com>
2017-02-19 00:04:41 +0100

    init

---
+++ 04/04.hs
@@ -0,0 +1,15 @@
+-- e1
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (\x -> x - 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n = n + fun2 (n ‘div‘ 2)
+  | otherwise = fun2 (3 * n + 1)
