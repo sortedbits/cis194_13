c8dabc2cb7da1571b76ebe38e7ed050d8ed2418f
sortedbits <sortedbit@zoho.com>
2017-02-26 17:09:37 +0100

    init

--- 06/Fibonacci.hs
+++ 06/Fibonacci.hs
@@ -12,3 +12,10 @@ fibs1 :: [Integer]
 fibs1 = map fib [0..] 
 
 -- e2
+fibs2 :: [Integer] 
+fibs2 =  1 : (1 : (fib' 2 1 1))
+
+fib' :: Integer -> Integer -> Integer -> [Integer]
+fib' x x0 x1 = x2 : (fib' (x + 1) x1 x2) where
+  x2 = x0 + x1
+  
